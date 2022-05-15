19ffe77855e1ac4303659581525031b2a343b97b
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-15 15:04:53 -0600

    creating new repo

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+    //SET INTERVAL WITH MOMENT.JS
+
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+    //BUILD THIS USING TABLE
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+    //ASSIGNING PAST PRESENT AND FUTURE IRL TIME TO A COLOR, IF/ELSE STATEMENTS:
+
+// WHEN I click into a time block
+// THEN I can enter an event
+    //MODUALS
+
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+    //LOCAL STORAGE/JSON
+
+// WHEN I refresh the page
+// THEN the saved events persist
