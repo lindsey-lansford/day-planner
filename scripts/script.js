084c2798563320a4ce1d4c2cebb34ab6c11fbf07
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-17 23:05:39 -0600

    declared vars and styled css

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,42 @@
 
-let currentDate = $("#current-date");
-
+const currentDate = $("#current-date");
 currentDate.text(moment().format("dddd, MMMM Do YYYY"));
 
-const pastGray = document.querySelector('.past').style.color = "gray";
-const presentRed = document.querySelector('.present').style.color = "red";
-const futureGreen = document.querySelector('.future').style.color = "green";
+const past = $(".past");
+const present = $(".present");
+const future = $(".future");
+
+const nineA = $("9");
+const tenA = $("10");
+const elevenA = $("11");
+const twelveP = $("12");
+const oneP = $("1");
+const twoP = $("2");
+const threeP = $("3");
+const fourP = $("4");
+const fiveP = $("5");
+
+
+const workDayTotal = [nineA, tenA, elevenA, twelveP, oneP, twoP, threeP, fourP, fiveP]
+console.log(workDayTotal)
+
+var currentTime = moment().format('HH');
+console.log(currentTime)
+
+//do i need to add the 8workday hours and 24total day hours to a loop or varible??
+for (let i = 0; i < workDayTotal.length; i++) {
+    console.log([i])
+    if (currentTime === i) {
+        currentTime.attr('present'); 
+    }
+    // if (currentTime > i) {
+    //     currentTime.attr('future');
+    // }
+    // else {
+    //     currentTime.attr('past');
+    // }
+
+}
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
